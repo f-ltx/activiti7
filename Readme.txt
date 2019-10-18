8c019e7977fc062487c004a52b5d8388fa31b788
litianxiang <litianxiang@topnet.net.cn>
2019-10-18 17:57:29 +0800

    测试thymeleaf

--- Readme.txt
+++ Readme.txt
@@ -2,3 +2,4 @@
 2、配置mysql和oracle的数据源
 3、加入activiti7
 4、屏蔽spring-security
+5、springboot版本必须是2.1.9
--- src/main/java/cn/ltx/activiti7/controller/HelloController.java
+++ src/main/java/cn/ltx/activiti7/controller/HelloController.java
@@ -2,25 +2,22 @@ package cn.ltx.activiti7.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * Created by admin on 17/6/16.
  */
 
-@RestController
-public class IndexController {
+@Controller
+public class HelloController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @GetMapping("/index")
+    @GetMapping("/hello")
     public Object index() {
-        logger.debug("debug");
-        logger.info("info");
-        logger.warn("warn");
-        logger.error("error");
-        return "success";
+        logger.debug("hello");
+        return "hello";
     }
 
 }
